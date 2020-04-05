a10caa4848dc441d3a37bfabf4e3e91af71a7fa5
Toshiki Iga <igapyon@gmail.com>
2020-04-05 12:38:00 +0900

    adding src

---
+++ src/main/java/jp/igapyon/cityinfojp/CityinfojpApplication.java
@@ -0,0 +1,13 @@
+package jp.igapyon.cityinfojp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityinfojpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityinfojpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/igapyon/cityinfojp/CityinfojpApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.igapyon.cityinfojp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityinfojpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
