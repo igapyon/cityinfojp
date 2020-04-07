22c131a7d27019bcb21dfa8d9edc072f27b754d7
Toshiki Iga <igapyon@gmail.com>
2020-04-07 22:18:23 +0900

    sitemap

---
+++ src/test/java/jp/igapyon/cityinfojp/sitemap/MySitemapGeneratorTest.java
@@ -0,0 +1,11 @@
+package jp.igapyon.cityinfojp.sitemap;
+
+import org.junit.jupiter.api.Test;
+
+class MySitemapGeneratorTest {
+
+    @Test
+    void test() throws Exception {
+        MySitemapGenerator.main(new String[] { "this" });
+    }
+}
