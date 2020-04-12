48f20b624ea60de086dd5738adbfd1c3a39e24bb
Toshiki Iga <igapyon@gmail.com>
2020-04-12 10:07:58 +0900

    save

---
+++ src/main/java/jp/igapyon/cityinfojp/dyn/DataEntryController.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 Toshiki Iga
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.igapyon.cityinfojp.dyn;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jp.igapyon.cityinfojp.dyn.fragment.JumbotronFragmentBean;
+import jp.igapyon.cityinfojp.dyn.fragment.navbar.NavbarBean;
+
+@Controller
+public class DataEntryController {
+    @GetMapping({ "/dyn/dataentry.html" })
+    public String index(Model model) throws IOException {
+
+        model.addAttribute("jumbotron", getJumbotronBean());
+
+        model.addAttribute("navbar", getNavbarBean());
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        model.addAttribute("processDateTime", dtf.format(LocalDateTime.now()));
+
+        return "dyn/dataentry";
+    }
+
+    public static JumbotronFragmentBean getJumbotronBean() {
+        JumbotronFragmentBean jumbotron = new JumbotronFragmentBean();
+        jumbotron.setTitle("Data Entry");
+        return jumbotron;
+    }
+
+    public static NavbarBean getNavbarBean() {
+        NavbarBean navbar = NavbarUtil.buildNavbar();
+        navbar.getItemList().get(4).setCurrent(true);
+        return navbar;
+    }
+}
