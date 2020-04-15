bd14f8e18fed52c8d42b48f52cebab3d06a79ca8
Toshiki Iga <igapyon@gmail.com>
2020-04-16 08:05:57 +0900

    save

---
+++ src/test/java/jp/igapyon/cityinfojp/input/entry/PrefUrlEntryUtilTest.java
@@ -0,0 +1,17 @@
+package jp.igapyon.cityinfojp.input.entry;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class PrefUrlEntryUtilTest {
+
+    @Test
+    void test() throws IOException {
+        List<PrefUrlEntry> entryList = PrefUrlEntryUtil.readEntryListFromClasspath();
+        for (PrefUrlEntry entry : entryList) {
+            System.err.println(entry.getName() + ":" + entry.getUrl().get(0));
+        }
+    }
+}
