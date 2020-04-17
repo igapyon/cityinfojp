3c16996bc446b691c652a97cf95bf5a8a6bea4c9
Toshiki Iga <igapyon@gmail.com>
2020-04-18 07:24:14 +0900

    ホームページを調整 (#62)
    * ホームページを調整 
    * https://github.com//issues/50

---
+++ src/main/java/jp/igapyon/cityinfojp/dyn/DisplayAreaEntry.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Toshiki Iga
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.igapyon.cityinfojp.dyn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * HTMLレンダリング用のエントリ
+ * 
+ * @author Toshiki Iga
+ */
+public class DisplayAreaEntry {
+    private String name;
+    private String nameen;
+    private String url;
+    private List<DisplayPrefEntry> prefList = new ArrayList<>();
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameen() {
+        return nameen;
+    }
+
+    public void setNameen(String nameen) {
+        this.nameen = nameen;
+    }
+
+    public List<DisplayPrefEntry> getPrefList() {
+        return prefList;
+    }
+}
---
+++ src/main/java/jp/igapyon/cityinfojp/input/entry/AreaEntry.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2020 Toshiki Iga
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.igapyon.cityinfojp.input.entry;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "name", "nameen", "pref" })
+public class AreaEntry {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("nameen")
+    private String nameen;
+    @JsonProperty("pref")
+    private List<String> pref = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("nameen")
+    public String getNameen() {
+        return nameen;
+    }
+
+    @JsonProperty("nameen")
+    public void setNameen(String nameen) {
+        this.nameen = nameen;
+    }
+
+    @JsonProperty("pref")
+    public List<String> getPref() {
+        return pref;
+    }
+
+    @JsonProperty("pref")
+    public void setPref(List<String> pref) {
+        this.pref = pref;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
